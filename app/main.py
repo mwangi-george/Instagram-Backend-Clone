12b6c73e781a197d191a313f8ad8050bedc11dbd
George Mwangi <mwangigeorge648@gmail.com>
2024-10-03 22:16:05 +0300

    Include home router

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from .routes import home_router
 
 
 def create_app_entry_point() -> FastAPI:
@@ -8,8 +9,14 @@ def create_app_entry_point() -> FastAPI:
         title="Instagram Backend",
         description="Instagram Backend Clone",
         version="1.0.0",
-
     )
+
+    # initialize routers
+    home_routes = home_router()
+
+    # include routers in the entry point
+    entry_point.include_router(home_routes)
+
     return entry_point
 
 
