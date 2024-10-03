05d3e0af0d695e8d6361ac2c8c5b7e9af43e6ddc
George Mwangi <mwangigeorge648@gmail.com>
2024-10-03 22:15:37 +0300

    Injected urls dynamically

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,6 +1,11 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from dotenv import load_dotenv
+import os
+
+# load environment variables
+load_dotenv()
 
 
 def create_homepage_router() -> APIRouter:
@@ -13,8 +18,15 @@ def create_homepage_router() -> APIRouter:
     # create a template object
     template = Jinja2Templates(directory="templates")
 
+    # Swagger documentation
+    swagger_url = os.getenv("API_SWAGGER_URL")
+
     @router.get("/", response_class=HTMLResponse)
     async def homepage(request: Request):
-        return template.TemplateResponse(request=request, name="index.html")
+        return template.TemplateResponse(
+            request=request,
+            name="index.html",
+            context={"swagger_url": swagger_url}
+        )
 
     return router
