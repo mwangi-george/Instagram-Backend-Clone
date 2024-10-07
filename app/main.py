cf4db69b369afce0d1f1884f67b5c667bf30cefe
George Mwangi <mwangigeorge648@gmail.com>
2024-10-07 17:32:17 +0300

    Added user creation route to main file

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from .routes import home_router
+from .routes import home_router, user_router
 
 
 def create_app_entry_point() -> FastAPI:
@@ -13,10 +13,13 @@ def create_app_entry_point() -> FastAPI:
 
     # initialize routers
     home_routes = home_router()
+    user_routes = user_router()
 
     # include routers in the entry point
     entry_point.include_router(home_routes)
+    entry_point.include_router(user_routes)
 
+    # return entrypoint object
     return entry_point
 
 
