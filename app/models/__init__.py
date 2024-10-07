32a70d10520ebc0bb777d152183e368470bf33b5
George Mwangi <mwangigeorge648@gmail.com>
2024-10-07 17:30:42 +0300

    Initialization files

---
+++ app/models/__init__.py
@@ -0,0 +1,8 @@
+from .base import Base, get_db
+from .commen_likes import CommentLike
+from .comments import Comment
+from .followers import Follower
+from .likes import Like
+from .password_resets import PasswordReset
+from .posts import Post
+from .users import User
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,1 +1,2 @@
 from .home import create_homepage_router as home_router
+from .users import create_user_router as user_router
