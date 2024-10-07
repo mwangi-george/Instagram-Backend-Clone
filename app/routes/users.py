f837ed710d6f083b83a5a69e8bc4714a43105aa4
George Mwangi <mwangigeorge648@gmail.com>
2024-10-07 19:03:07 +0300

    Wrote code comments

--- app/routes/users.py
+++ app/routes/users.py
@@ -17,12 +17,14 @@ def create_user_router() -> APIRouter:
     # instantiate UserServices class with implementation logic
     user_services = UserServices()
 
+    # ------------------User Sign Up--------------------------
     @router.post(path="/signup", response_model=ActionConfirm, status_code=status.HTTP_201_CREATED)
     async def create_user(user_data: UserCreate, db: Session = Depends(get_db)):
         response = user_services.create_user(user_data, db)
         formated_response = ActionConfirm(msg=response)
         return formated_response
 
+    # ------------------User Login--------------------------
     @router.post(path="/login", response_model=TokenCreate, status_code=status.HTTP_200_OK)
     async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
         token = user_services.login_user(form_data.username, form_data.password, db)
