3f2c755dcbc47d2a153ffb1f7e6018356d036b9d
George Mwangi <mwangigeorge648@gmail.com>
2024-10-25 17:39:24 +0300

    Added password reset route

--- app/routes/users.py
+++ app/routes/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, BackgroundTasks
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from ..models import get_db
@@ -7,7 +7,7 @@ from ..schemas.users import (
     ActionConfirm,
     TokenCreate,
     UserCreate,
-
+    PasswordResetRequest,
 )
 
 
@@ -32,5 +32,15 @@ def create_user_router() -> APIRouter:
     async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
         token = user_services.login_user(form_data.username, form_data.password, db)
         return token
+
+    @router.post(path="/password-reset-request", response_model=ActionConfirm, status_code=status.HTTP_200_OK)
+    async def request_password_reset(recipient: PasswordResetRequest, background_tasks: BackgroundTasks, db: Session = Depends(get_db)):
+        confirmation_message = await user_services.send_password_reset_email(recipient.email, background_tasks, db)
+        formated_response = ActionConfirm(msg=confirmation_message)
+        return formated_response
+
+    @router.post(path="/confirm-password-reset", status_code=status.HTTP_200_OK)
+    async def confirm_password_reset():
+        pass
 
     return router
