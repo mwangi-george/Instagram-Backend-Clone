ce1eb95c8662d16955c11c6062a3fe8016246f1c
George Mwangi <mwangigeorge648@gmail.com>
2024-10-26 19:21:06 +0300

    Changed func names

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,2 +1,3 @@
 from .home import create_homepage_router as home_router
-from .users import create_user_router as user_router
+from .auth import create_auth_router as user_router
+from .user_profile import create_user_profile_router as user_profile_router
